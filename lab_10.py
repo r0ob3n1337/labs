628878a70f423f3c2b0a7014e81d69a750f2e486
r0ob3n1337 <r00b3n1337@gmail.com>
2024-03-17 16:41:41 +0300

    Add lab-10-3

--- lab_10.py
+++ lab_10.py
@@ -67,4 +67,34 @@ def lab_10_1():
         print("[ОШИБКА] Проблемы с файламом JSON.")
 
 
-lab_10_1()
+def lab_10_3():
+    """Чтение и запись словаря"""
+    en_ru_path = os.path.join(os.getcwd(), "lab_10", "en_ru.txt")
+    ru_en_path = os.path.join(os.getcwd(), "lab_10", "ru_en.txt")
+
+    eng_dict = {}
+    ru_dict = {}
+
+    try:
+        with open(en_ru_path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                (eng_w_str, ru_w_str) = line.split(" - ")
+                ru_words = ru_w_str.replace("\n", "").split(", ")
+                eng_dict[eng_w_str] = ru_words
+
+                for rw in ru_words:
+                    if rw in ru_dict:
+                        ru_dict[rw].append(eng_w_str)
+                    else:
+                        ru_dict[rw] = [eng_w_str]
+
+        with open(ru_en_path, "w+", encoding="utf-8") as f:
+            for ru_w, eng_w in sorted(ru_dict.items()):
+                f.write(f"{ru_w} - {", ".join(eng_w)}\n")
+
+    except OSError:
+        print("[ОШИБКА] Проблемы с файлом.")
+
+
+lab_10_3()
