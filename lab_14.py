9055693f69c3e9fcc34083a237b5d548c3b53d2d
r0ob3n1337 <r00b3n1337@gmail.com>
2024-05-05 14:05:03 +0300

    Update calc for lab-14

--- lab_14.py
+++ lab_14.py
@@ -18,18 +18,17 @@ OPERATIONS = {
 
 
 class Calculator(QMainWindow):
+    """Калькулятор"""
+
     def __init__(self):
         super(Calculator, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.a = None
-        self.b = None
+        self.a_nums = []
+        self.b_nums = []
+        self.operation = None
         self.total = None
-
-        self.input = self.ui.input
-        self.result = self.ui.lbl_result
-        self.input_max_len = self.input.maxLength()
 
         self.connect_numbers_buttons()
         self.connect_clear_buttons()
@@ -39,16 +38,16 @@ class Calculator(QMainWindow):
 
     def connect_numbers_buttons(self) -> None:
         """Обработка на клавиши-цифры"""
-        self.ui.btn_0.clicked.connect(lambda: self.add_number("0"))
-        self.ui.btn_1.clicked.connect(lambda: self.add_number("1"))
-        self.ui.btn_2.clicked.connect(lambda: self.add_number("2"))
-        self.ui.btn_3.clicked.connect(lambda: self.add_number("3"))
-        self.ui.btn_4.clicked.connect(lambda: self.add_number("4"))
-        self.ui.btn_5.clicked.connect(lambda: self.add_number("5"))
-        self.ui.btn_6.clicked.connect(lambda: self.add_number("6"))
-        self.ui.btn_7.clicked.connect(lambda: self.add_number("7"))
-        self.ui.btn_8.clicked.connect(lambda: self.add_number("8"))
-        self.ui.btn_9.clicked.connect(lambda: self.add_number("9"))
+        self.ui.btn_0.clicked.connect(lambda: self.press_number("0"))
+        self.ui.btn_1.clicked.connect(lambda: self.press_number("1"))
+        self.ui.btn_2.clicked.connect(lambda: self.press_number("2"))
+        self.ui.btn_3.clicked.connect(lambda: self.press_number("3"))
+        self.ui.btn_4.clicked.connect(lambda: self.press_number("4"))
+        self.ui.btn_5.clicked.connect(lambda: self.press_number("5"))
+        self.ui.btn_6.clicked.connect(lambda: self.press_number("6"))
+        self.ui.btn_7.clicked.connect(lambda: self.press_number("7"))
+        self.ui.btn_8.clicked.connect(lambda: self.press_number("8"))
+        self.ui.btn_9.clicked.connect(lambda: self.press_number("9"))
 
     def connect_clear_buttons(self) -> None:
         """Обработка кликов на кнопки очистки поля и результатов"""
@@ -65,73 +64,110 @@ class Calculator(QMainWindow):
 
     def connect_math_operations(self) -> None:
         """Обработка клика по кнпокам математических операций"""
-        self.ui.btn_plus.clicked.connect(lambda: self.add_temp_operation("+"))
-        self.ui.btn_minus.clicked.connect(lambda: self.add_temp_operation("-"))
-        self.ui.btn_divide.clicked.connect(lambda: self.add_temp_operation("/"))
-        self.ui.btn_mult.clicked.connect(lambda: self.add_temp_operation("*"))
+        self.ui.btn_plus.clicked.connect(lambda: self.press_operation("+"))
+        self.ui.btn_minus.clicked.connect(lambda: self.press_operation("-"))
+        self.ui.btn_divide.clicked.connect(lambda: self.press_operation("/"))
+        self.ui.btn_mult.clicked.connect(lambda: self.press_operation("*"))
+
+    def clear_temp_vars(self) -> None:
+        self.a_nums = []
+        self.b_nums = []
+        self.operation = None
+        self.total = None
 
     def clear_all(self) -> None:
         """Очистка поля и результатов"""
-        self.result.clear()
-        self.input.setText("0")
+        self.ui.lbl_result.clear()
+        self.ui.input.setText("0")
+        self.clear_temp_vars()
 
     def clear_input(self) -> None:
         """Очистка поля"""
-        self.input.setText("0")
+        self.ui.input.setText("0")
 
-    def add_number(self, btn_text: str) -> None:
+    def press_number(self, number: str) -> None:
         """Добавляем цифру в инпут при нажатии на кнопку"""
-        if self.input.text() == "0":
-            self.input.setText(btn_text)
+        if self.operation:
+            self.b_nums.append(number)
+            self.ui.input.setText("".join(self.b_nums))
         else:
-            self.input.setText(self.input.text() + btn_text)
+            self.a_nums.append(number)
+            self.ui.input.setText("".join(self.a_nums))
 
     def add_dot(self) -> None:
         """Добавляем точку в инпут при нажатии на кнопку, если ее еще нет"""
-        if "." not in self.input.text():
-            self.input.setText(self.input.text() + ".")
+        if "." not in self.ui.input.text():
+            self.ui.input.setText(self.ui.input.text() + ".")
 
-    def add_temp_operation(self, operation: str):
+    def press_operation(self, operation: str):
         """Запись математической операции в лейбл"""
-        if not self.ui.lbl_result.text():
-            self.ui.lbl_result.setText(self.ui.input.text() + f" {operation}")
-            self.ui.input.setText("0")
+
+        if self.total:
+            self.a_nums = self.total.split()
+
+        self.operation = operation
+        self.ui.lbl_result.setText("".join(self.a_nums) + f" {operation}")
+        self.ui.input.setText("0")
 
     def remove_extra_zeros(self, num: Union[float, int, str]) -> str:
+        """Очистка лишни нулей"""
         n = str(float(num))
         return n.replace(".0", "") if n.endswith(".0") else n
 
-    def get_num(self) -> Union[int, float]:
+    def get_right_side_nums(self) -> Union[int, float]:
         """Берем значение из инпута с введенными"""
-        input_text = self.ui.input.text().strip(".")
-        return float(input_text) if "." in input_text else int(input_text)
+        if not self.a_nums:
+            return 0
 
-    def get_temp_num(self) -> Union[float, int, None]:
+        nums = "".join(self.a_nums)
+        return float(nums) if "." in nums else int(nums)
+
+    def get_left_side_nums(self) -> Union[float, int, None]:
         """Если текст есть в лейбле операций, то берем оттуда число ДО знака"""
-        if self.ui.lbl_result.text():
-            temp = self.ui.lbl_result.text().strip(".").split()[0]
-            return float(temp) if "." in temp else int(temp)
+        if not self.b_nums:
+            return 0
 
-    def get_math_operation_symbol(self) -> Optional[str]:
+        nums = "".join(self.b_nums)
+        return float(nums) if "." in nums else int(nums)
+
+    def get_operation(self) -> Optional[str]:
         """Если текст есть в лейбле операций, то берем оттуда знак операции"""
-        if self.ui.lbl_result.text():
-            return self.ui.lbl_result.text().strip(".").split()[-1]
+        if self.operation:
+            return self.operation
+
+    def clear_operation(self):
+        """Очистка операции"""
+        self.operation = None
 
     def calculate(self) -> Optional[str]:
         """Основная функция калькуляции"""
         if self.ui.input.text():
-            result = self.remove_extra_zeros(
-                str(
-                    OPERATIONS[self.get_math_operation_symbol()](
-                        self.get_temp_num(), self.get_num()
+            try:
+                result = self.remove_extra_zeros(
+                    str(
+                        OPERATIONS[self.get_operation()](
+                            self.get_right_side_nums(), self.get_left_side_nums()
+                        )
                     )
                 )
-            )
-            self.ui.lbl_result.setText(
-                f"{self.get_temp_num()} {self.get_math_operation_symbol()} {self.get_num()} = {result}"
-            )
-            self.ui.input.setText(result)
-            return result
+                self.ui.lbl_result.setText(
+                    f"{self.get_right_side_nums()} {self.get_operation()} {self.get_left_side_nums()} = {result}"
+                )
+
+                self.total = result
+                self.ui.input.setText(result)
+                self.clear_operation()
+                self.a_nums = []
+                self.b_nums = []
+
+                return result
+            except KeyError:
+                self.ui.lbl_result.setText("")
+                self.ui.input.setText("0")
+            except ZeroDivisionError:
+                self.ui.lbl_result.setText("Деление на ноль")
+                self.ui.input.setText("0")
+                self.clear_all()
 
 
 if __name__ == "__main__":
